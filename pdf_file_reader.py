df47d4b300a086b881363a9015a5eaa9b224b654
Arnav Shekaran <arnard76@gmail.com>
2023-03-09 22:34:21 +1300

    printing the outline with level-1 subtopics

--- pdf_file_reader.py
+++ pdf_file_reader.py
@@ -1,19 +1,23 @@
 from PyPDF2 import PdfReader, PdfWriter
+import json
 filename = "coursebook_S12023.pdf"
 
 with open(filename, "rb") as f:
     r = PdfReader(f)
-    
-    # bookmarks = list(map(lambda x: (x, r.get_destination_page_number(x)), r.outline))
-    print(r.outline)
-    # for i, b in enumerate(bookmarks):
-        # begin = b[1]
-        # end  = bookmarks[i+1][1] if i < len(bookmarks) - 1 else len(r.pages)
-        # # print(len(r.pages[begin:end]))
-        # name = b[0] + ".pdf"
-        # print(f"{name=}: {begin=}, {end=}")
-        # with open(name, "wb") as f:
-        #     w = PdfWriter(f)
-        #     for p in r.pages[begin:end]:
-        #         w.add_page(p)
-        #     w.write(f)
+
+    # formatting outline into parent topic > children topic 
+    outline = []
+    last_parent =None
+    for index, element in enumerate(r.outline):
+        # element could be a topic or a sub-topic
+        if index%2==0:
+            last_parent = element
+        else: 
+            for index, child in enumerate(element):
+                element[index] = child['/Title']
+            outline.append({'name': last_parent.node['/Title'], 'childs': element})
+
+    print(outline)
+
+    with open('out.json', 'w') as outline_file:
+        json.dump(outline, outline_file)
