ae51bea252b405c6026135c80d9c2a4a91e61a77
Arnav Shekaran <arnard76@gmail.com>
2023-03-10 22:26:10 +1300

    better finding of last page
    but there is a bug here! it will take the page where the next topic is.
    
    this works for 225 because the next topic could be half way down the page
    
    but for 220 the next topics always start on a new page
    so for 220 the content will always include some of the next chapters 😢

--- api/pdf_file_reader.py
+++ api/pdf_file_reader.py
@@ -17,13 +17,30 @@ with open(filename, "rb") as f:
         if not type(element)==list:
             last_parent = element
         else: 
-            for index, child in enumerate(element):
-                child_page = r.get_destination_page_number(child)
-                print(child_page, type(child_page))
-                page = r.pages[child_page]
-                text_for_child_topic = page.extract_text()
-                print(text_for_child_topic)
-                element[index] = {'name': child['/Title'], "content": text_for_child_topic}
+            for idx, child in enumerate(element):
+                first_page = r.get_destination_page_number(child)
+                if index==len(r.outline)-1:
+                    last_page = len(r.pages)-1
+                elif idx==len(element)-1:
+                    if type(r.outline[index+1])==list:
+                        if len(r.outline[index+1]) > 0:
+                            last_page_dest = r.outline[index+1][0]
+                        else:
+                            last_page_dest = len(r.pages)-1
+                    else:
+                        last_page_dest = r.outline[index+1]
+
+                    last_page = r.get_destination_page_number(last_page_dest)
+
+                else:
+                    last_page = r.get_destination_page_number(element[idx+1])
+
+                text_for_child_topic=""
+                for pageNum in range(first_page, last_page+1):
+                    page = r.pages[pageNum] 
+                    text_for_child_topic+=page.extract_text()+"\n\n\n\n\n"
+
+                element[idx] = {'name': child['/Title'], "content": text_for_child_topic}
 
 
             outline.append({
