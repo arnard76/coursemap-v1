fdc8c1528358a5416c0f49b1ae81afacbff68955
Arnav Shekaran <arnard76@gmail.com>
2023-03-10 14:35:21 +1300

    extracting the content as well 😁😁
    on the page number of the outline link

--- api/pdf_file_reader.py
+++ api/pdf_file_reader.py
@@ -11,13 +11,21 @@ with open(filename, "rb") as f:
     # formatting outline into parent topic > children topic 
     outline = []
     last_parent =None
+  
     for index, element in enumerate(r.outline):
         # element could be a topic or a sub-topic
         if index%2==0:
             last_parent = element
         else: 
             for index, child in enumerate(element):
-                element[index] = child['/Title']
+                child_page = r.get_destination_page_number(child)
+                print(child_page, type(child_page))
+                page = r.pages[child_page]
+                text_for_child_topic = page.extract_text()
+                print(text_for_child_topic)
+                element[index] = {'name': child['/Title'], "content": text_for_child_topic}
+
+
             outline.append({
                 'name': last_parent.node['/Title'], 
                 'definition': {'simple': define_topic(last_parent.node['/Title'])},
