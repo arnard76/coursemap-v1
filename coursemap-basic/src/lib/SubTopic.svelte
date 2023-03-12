1c39aa4f638fb806413f43d4446fe201a9c8ea07
Arnav Shekaran <arnard76@gmail.com>
2023-03-13 10:43:47 +1300

    now topics can be sub topics too
    i.e. the content can be displayed for the entire topic rather than the
    smaller subtopics

--- coursemap-basic/src/lib/SubTopic.svelte
+++ coursemap-basic/src/lib/SubTopic.svelte
@@ -3,17 +3,22 @@
   export let topicName = "";
   export let subTopicName = "";
 
+  export let subTopicWhole = null;
+  export let backLink = null;
+
   import { courses } from "./outlines";
   import Circle from "./Circle.svelte";
 
-  $: subTopic = courses
-    .find((course) => course.id === courseId)
-    .outline.find((topic) => topic.name === topicName)
-    .childs.find((child) => child["name"] === subTopicName);
+  $: subTopic =
+    subTopicWhole ||
+    courses
+      .find((course) => course.id === courseId)
+      .outline.find((topic) => topic.name === topicName)
+      .childs.find((child) => child["name"] === subTopicName);
 </script>
 
 <div style="position:relative;">
-  <a href={"/" + courseId + "/" + topicName}>
+  <a href={backLink || "/" + courseId + "/" + topicName}>
     <Circle radius={2} />
   </a>
   <p>{subTopic.name}</p>
--- coursemap-basic/src/lib/Topic.svelte
+++ coursemap-basic/src/lib/Topic.svelte
@@ -1,12 +1,33 @@
 <script>
   import RingOfTopics from "./RingOfTopics.svelte";
-  import { example as outline } from "./outlines";
+  import { courses } from "./outlines";
+  import SubTopic from "./SubTopic.svelte";
+
   export let topicName;
   export let courseId;
+
+  $: selectedTopic = courses
+    .find((course) => course.id === courseId)
+    .outline.find((topic) => topic.name === topicName);
+
+  let hasSubTopics = false;
+  $: if (selectedTopic.childs?.length > 0) {
+    hasSubTopics = true;
+  } else {
+    hasSubTopics = false;
+  }
 </script>
 
-<p>{topicName}</p>
 <!-- <div style="position:relative;">
   <p>{topic.definition.simple}</p>
 </div> -->
-<RingOfTopics {topicName} {courseId} />
+{#if hasSubTopics}
+  <p>{topicName}</p>
+  <RingOfTopics {topicName} {courseId} />
+{:else}
+  <SubTopic
+    subTopicWhole={selectedTopic}
+    backLink={"/" + courseId + "/"}
+    {courseId}
+  />
+{/if}
